c23773dc1734bfc12ce6a26daed16ce864891072
phlppchrtn <philippe.chretien@kleegroup.com>
2021-03-30 19:26:52 +0200

    clean

--- src/main/java/io/vertigo/ai/bb/BlackBoard.java
+++ src/main/java/io/vertigo/ai/bb/BlackBoard.java
@@ -8,25 +8,25 @@ public interface BlackBoard {
 	//--- Keys
 	//------------------------------------
 	/**
-	 * Returns if the keys exist
+	 * Returns if the keys exists
 	 *
 	 * @param key the key
 	 * @return if the key exists
 	 */
-	public boolean exists(final String key);
+	boolean exists(final String key);
 
 	/**
 	 * Returns all the keys matching the pattern
 	 * @param keyPattern the pattern
 	 * @return A list of keys
 	 */
-	public Set<String> keys(final String keyPattern);
+	Set<String> keys(final String keyPattern);
 
-	public Set<String> keys();
+	Set<String> keys();
 
-	public void removeAll();
+	void removeAll();
 
-	public void remove(final String keyPattern);
+	void remove(final String keyPattern);
 
 	//------------------------------------
 	//--- KV
@@ -37,51 +37,51 @@ public interface BlackBoard {
 	 * @param key the key
 	 * @return the value mapped with the key or null if the key does not exist
 	 */
-	public String getString(final String key);
+	String getString(final String key);
 
-	public Integer getInteger(final String key);
+	Integer getInteger(final String key);
 
-	public void putInteger(final String key, final Integer value);
+	void putInteger(final String key, final Integer value);
 
-	public void putString(final String key, final String value);
+	void putString(final String key, final String value);
 
-	public String format(final String msg);
+	String format(final String msg);
 
-	public void append(final String key, final String something);
+	void append(final String key, final String something);
 
-	public void decr(final String key);
+	void decr(final String key);
 
-	public void incr(final String key);
+	void incr(final String key);
 
-	public void incrBy(final String key, final int value);
+	void incrBy(final String key, final int value);
 
 	//String
-	public boolean eq(final String key, final String compare);
+	boolean eq(final String key, final String compare);
 
-	public boolean eqCaseInsensitive(final String key, final String compare);
+	boolean eqCaseInsensitive(final String key, final String compare);
 
-	public boolean startsWith(final String key, final String compare);
+	boolean startsWith(final String key, final String compare);
 
 	//Integer
-	public boolean lt(final String key, final Integer compare);
+	boolean lt(final String key, final Integer compare);
 
-	public boolean eq(final String key, final Integer compare);
+	boolean eq(final String key, final Integer compare);
 
-	public boolean gt(final String key, final Integer compare);
+	boolean gt(final String key, final Integer compare);
 
 	//------------------------------------
 	//- List                             -
 	//- All methods are prefixed with list  -
 	//------------------------------------
 
-	public int listLen(final String key);
+	int listLen(final String key);
 
-	public void listPush(final String key, final String value);
+	void listPush(final String key, final String value);
 
-	public String listPop(final String key);
+	String listPop(final String key);
 
-	public String listPeek(final String key);
+	String listPeek(final String key);
 
-	public String listGet(final String key, final int idx);
+	String listGet(final String key, final int idx);
 
 }
