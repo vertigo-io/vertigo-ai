10441463e782c18595287c8c2107d5e32c741b8a
phlppchrtn <philippe.chretien@kleegroup.com>
2021-03-31 16:42:51 +0200

    doc

--- src/main/java/io/vertigo/ai/bb/BlackBoardManager.java
+++ src/main/java/io/vertigo/ai/bb/BlackBoardManager.java
@@ -1,14 +1,31 @@
 package io.vertigo.ai.bb;
 
-import io.vertigo.core.node.component.Component;
+import io.vertigo.core.node.component.Manager;
 
-public interface BlackBoardManager extends Component {
+/**
+ * This manager provides blackboards. 
+ * You have to connect with a blackboard by its name.
+ * 
+ * You can copy a tree of keys (with values) from a blackboard to another.
+ * 
+ * @author pchretien
+ */
+public interface BlackBoardManager extends Manager {
 	String STORE_NAME_REGEX = "[a-z]+";
 
 	String MAIN_STORE_NAME = "main";
 
+	/**
+	 * Connects with a blackboard identified by its name.
+	 * @param storeName the name of the blackboard
+	 * @return the blackboard
+	 */
 	BlackBoard connect(String storeName);
 
+	/**
+	 * Connects with the main blackboard
+	 * @return the main blackboard
+	 */
 	default BlackBoard connect() {
 		return connect(MAIN_STORE_NAME);
 	}
