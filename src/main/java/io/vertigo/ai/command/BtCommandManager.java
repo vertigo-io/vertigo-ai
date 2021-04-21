df686004b5b22d3c4f9aba702d8c9206f7c9d527
phlppchrtn <philippe.chretien@kleegroup.com>
2021-04-21 19:31:51 +0200

    respect the law

---
+++ src/main/java/io/vertigo/ai/command/BtCommandManager.java
@@ -0,0 +1,25 @@
+package io.vertigo.ai.command;
+
+import java.util.List;
+import java.util.function.Function;
+
+import io.vertigo.ai.bt.BTNode;
+import io.vertigo.core.node.component.Manager;
+
+/**
+ * Command manager transforms "commands" as diverse input form into a BT.
+ * Commands are by nature extensible by Defnitions and can require additional parameters.
+ * Theses parameters are passed to the returned function that produces a BT.
+ *
+ * @author skerdudou
+ */
+public interface BtCommandManager extends Manager {
+
+	/**
+	 * Transform commands as input text.
+	 *
+	 * @param text input commands
+	 * @return the function to produce the BT
+	 */
+	Function<List<Object>, BTNode> parse(String text);
+}
