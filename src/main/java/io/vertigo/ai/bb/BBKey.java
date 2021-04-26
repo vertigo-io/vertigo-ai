968f5b2d75436dc8e95dd02376ecd89cf07332d3
phlppchrtn <philippe.chretien@kleegroup.com>
2021-04-26 23:49:09 +0200

    doc

--- src/main/java/io/vertigo/ai/bb/BBKey.java
+++ src/main/java/io/vertigo/ai/bb/BBKey.java
@@ -2,10 +2,29 @@ package io.vertigo.ai.bb;
 
 import io.vertigo.core.lang.Assertion;
 
+/**
+ * The key defines the location of a value.  
+ * A key can be composed of atomic keys (Ki).
+ * K1+K2+...Kn => K 
+ * K is a composite key (aka a path)
+ * Where 
+ *  - K1 is the head
+ *  - Kn is the tail
+ *  
+ * if K is atomic then head = tail = K
+ * 
+ * A Key can be composite or atomic.
+ * 
+ * ex :
+ * /u/name
+ * /game/score
+ * /game/players/1
+ * /game/players/2
+ * 
+ * @author pchretien
+ */
 public final class BBKey {
-
-	public static String KEY_REGEX = "/[a-z]+(/[a-z0-9]*)*";
-
+	public static String KEY_REGEX = "(/[a-z0-9]*)+";
 	private final String key;
 
 	private BBKey(final String key) {
@@ -16,51 +35,78 @@ public final class BBKey {
 		this.key = key;
 	}
 
-	public String key() {
-		return key;
-	}
-
 	public static BBKey of(final String key) {
 		return new BBKey(key);
 	}
 
-	public static BBKey of(final BBKey rootKey, final String key) {
-		Assertion.check()
-				.isNotNull(rootKey)
-				.isNotBlank(key);
-		//---
-		return BBKey.of(rootKey.key() + key);
+	public String key() {
+		return key;
 	}
+
+	//	public static BBKey of(final BBKey rootKey, final String key) {
+	//		Assertion.check()
+	//				.isNotNull(rootKey)
+	//				.isNotBlank(key);
+	//		//---
+	//		return BBKey.of(rootKey.key() + key);
+	//	}
 
 	public BBKey add(final BBKey otherKey) {
 		return BBKey.of(key + otherKey.key);
 	}
 
+	/**
+	 * Extracts the head (atomic Key) of a key.
+	 * A key can be composed of atomic keys (Ki).
+	 * K1+K2+...Kn => K 
+	 * K1 is the head
+	 * 
+	 * @return the head
+	 */
 	public BBKey head() {
 		Assertion.check()
 				.isTrue(key.charAt(0) == '/', "Key {0} doesn't start with the first char '/'", key);
+		//---
 		final int nextSlash = key.indexOf('/', 1);
-		return nextSlash < 0 ? this : BBKey.of(key.substring(0, nextSlash));
+		return nextSlash < 0
+				? this
+				: BBKey.of(key.substring(0, nextSlash));
 	}
 
+	/**
+	 * Extracts the tail (atomic Key) of a key.
+	 * A key can be composed of atomic keys (Ki).
+	 * K1+K2+...Kn => K 
+	 * Kn is the tail
+	 * 
+	 * @return the tail
+	 */
 	public BBKey tail() {
 		final int lastSlash = key.lastIndexOf('/');
-		return lastSlash == 0 ? this : BBKey.of(key.substring(lastSlash));
+		return lastSlash == 0
+				? this
+				: BBKey.of(key.substring(lastSlash));
 	}
 
-	public BBKey indent(final String prefix) {
-		Assertion.check().isNotBlank(prefix);
-		//---
-		return BBKey.of(prefix + key);
+	/**
+	 * @return true if the key is atomic 
+	 */
+	public boolean isAtomic() {
+		return key.lastIndexOf('/') == 0;
 	}
-
-	public BBKey outdent(final String prefix) {
-		Assertion.check()
-				.isNotBlank(prefix)
-				.isTrue(key.startsWith(prefix), "To outdent the key '{0}' it must starts with the provided prefix '{1}' ", key, prefix);
-		//---
-		return BBKey.of(key.substring(0, prefix.length() - 1));
-	}
+	//	public BBKey indent(final String prefix) {
+	//		Assertion.check().isNotBlank(prefix);
+	//		//---
+	//		return BBKey.of(prefix + key);
+	//	}
+	//
+	//	public BBKey outdent(final String prefix) {
+	//		Assertion.check()
+	//				.isNotBlank(prefix)
+	//				.isTrue(key.startsWith(prefix), "To outdent the key '{0}' it must starts with the provided prefix '{1}' ", key, prefix);
+	//		//---
+	//		return BBKey.of(key.substring(0, prefix.length() - 1));
+	//	}
 
 	@Override
 	public boolean equals(final Object obj) {
@@ -71,5 +117,4 @@ public final class BBKey {
 	public int hashCode() {
 		return key.hashCode();
 	}
-
 }
