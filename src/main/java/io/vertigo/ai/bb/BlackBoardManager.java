48270e5396ba7d3f50ae51bafc4fae823e515d53
phlppchrtn <philippe.chretien@kleegroup.com>
2021-04-28 11:17:50 +0200

    doc

--- src/main/java/io/vertigo/ai/bb/BlackBoardManager.java
+++ src/main/java/io/vertigo/ai/bb/BlackBoardManager.java
@@ -4,7 +4,7 @@ import io.vertigo.core.node.component.Manager;
 
 /**
  * This manager provides blackboards. 
- * You have to connect with a blackboard by its name.
+ * You have to connect to a blackboard by its name.
  * 
  * You can copy a tree of keys (with values) from a blackboard to another.
  * 
@@ -16,14 +16,14 @@ public interface BlackBoardManager extends Manager {
 	String MAIN_STORE_NAME = "main";
 
 	/**
-	 * Connects with a blackboard identified by its name.
+	 * Connects to a blackboard identified by its name.
 	 * @param storeName the name of the blackboard
 	 * @return the blackboard
 	 */
 	BlackBoard connect(String storeName);
 
 	/**
-	 * Connects with the main blackboard
+	 * Connects to the main blackboard
 	 * @return the main blackboard
 	 */
 	default BlackBoard connect() {
--- src/main/java/io/vertigo/ai/bt/BehaviorTreeManager.java
+++ src/main/java/io/vertigo/ai/bt/BehaviorTreeManager.java
@@ -2,8 +2,22 @@ package io.vertigo.ai.bt;
 
 import io.vertigo.core.node.component.Manager;
 
+/**
+ * Behavior Trees (or BT) are composed of nodes. 
+ * This composition brings modularity.
+ * 
+ * A the top of the tree there is ONE single node.
+ * To run the BT, you have to run this top node.
+ * 
+ * @author pchretien
+ */
 public interface BehaviorTreeManager extends Manager {
 
+	/**
+	 * Runs a BT.
+	 * @param rootNode the root node 
+	 * @return the status
+	 */
 	BTStatus run(BTNode rootNode);
 
 }
