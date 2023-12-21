b583435abcf36cb31f2dc719d009bb15f65038f1
Matthieu Laroche <matthieu.laroche@telecom-bretagne.eu>
2023-12-21 15:19:40 +0100

    Revert "rem bb"
    This reverts commit 84b703625ca9f2639ffe1c250a8a428b646ef28f.

---
+++ src/main/java/io/vertigo/ai/bb/BlackBoardManager.java
@@ -0,0 +1,33 @@
+package io.vertigo.ai.bb;
+
+import io.vertigo.core.node.component.Manager;
+
+/**
+ * This manager provides blackboards.
+ * You have to connect to a blackboard by its name.
+ *
+ * You can copy a tree of keys (with values) from a blackboard to another.
+ *
+ * @author pchretien
+ */
+public interface BlackBoardManager extends Manager {
+	String STORE_NAME_REGEX = "[a-z]+";
+
+	String MAIN_STORE_NAME = "main";
+
+	/**
+	 * Connects to a blackboard identified by its name.
+	 * @param storeName the name of the blackboard
+	 * @param rootKey the rootKey defining the subtree in which we are working
+	 * @return the blackboard
+	 */
+	BlackBoard connect(String storeName, BBKey rootKey);
+
+	/**
+	 * Connects to the main blackboard
+	 * @return the main blackboard
+	 */
+	default BlackBoard connect(final BBKey rootKey) {
+		return connect(MAIN_STORE_NAME, rootKey);
+	}
+}
